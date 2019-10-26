fec68b725bc03799b3540b254f6227ebf6e53a5d
Rafael Lindoso <rafael.lindoso@gmail.com>
2019-10-26 02:25:46 -0300

    Update do usuário

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -16,9 +16,27 @@ class UserController {
   }
 
   async update(req, res) {
-    console.log(req.userId);
+    const { email, oldPassword } = req.body;
 
-    return res.json({ ok: true });
+    const user = await User.findByPk(req.userId);
+
+    if (email !== user.email) {
+      const userExistis = await User.findOne({
+        where: { email },
+      });
+
+      if (userExistis) {
+        return res.status(400).json({ error: 'User already exists.' });
+      }
+    }
+
+    if (oldPassword && !(await user.checkPassword(oldPassword))) {
+      return res.status(401).json({ error: 'Password does not match' });
+    }
+
+    const { id, name, provider } = await user.update(req.body);
+
+    return res.json({ id, name, email, provider });
   }
 }
 
