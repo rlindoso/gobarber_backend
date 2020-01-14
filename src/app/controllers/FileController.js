a7cf43595507ed028aa2b83a694ff2fd27811fe0
rlindoso <rafael.lindoso@gmail.com>
2020-01-14 11:32:44 -0300

    swagger File

--- src/app/controllers/FileController.js
+++ src/app/controllers/FileController.js
@@ -14,3 +14,60 @@ class FileController {
 }
 
 export default new FileController();
+
+/**
+ * @swagger
+ * tags:
+ *   name: File
+ *   description: File
+ */
+
+/**
+ * @swagger
+ * path:
+ *  /file:
+ *    post:
+ *      security:
+ *        - bearerAuth: []
+ *      summary: Create file
+ *      tags: [File]
+ *      parameters:
+ *        - name: file
+ *          in: query
+ *          schema:
+ *            type: file
+ *          description: File image for avatar
+ *      responses:
+ *        "200":
+ *          description: A user schema
+ *          content:
+ *            application/json:
+ *              schema:
+ *                noExemple: noExemple
+ */
+
+ /**
+ * @swagger
+ * path:
+ *  /notifications/{id}:
+ *    put:
+ *      security:
+ *        - bearerAuth: []
+ *      summary: List Notifications
+ *      tags: [Notifications]
+ *      parameters:
+ *        - name: id
+ *          in: path
+ *          schema:
+ *            type: integer
+ *            format: integer,
+ *            default: 1
+ *          description: Id notification
+ *      responses:
+ *        "200":
+ *          description: A user schema
+ *          content:
+ *            application/json:
+ *              schema:
+ *                noExemple: noExemple
+ */
