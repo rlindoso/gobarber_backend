45b103cd0ed59eb8b995f8d6a409b8900446f21c
rlindoso <rafael.lindoso@gmail.com>
2020-01-27 12:40:09 -0300

    Campos virtuais no agendamento (past, cancelable)

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -16,7 +16,7 @@ class AppointmentController {
     const appointments = await Appointment.findAll({
       where: { user_id: req.userId, canceled_at: null },
       order: ['date'],
-      attributes: ['id', 'date'],
+      attributes: ['id', 'date', 'past', 'cancelable'],
       limit: 20,
       offset: (page - 1) * 20,
       include: [
@@ -149,8 +149,8 @@ class AppointmentController {
 
     await Queue.add(CancellationMail.key, {
       appointment,
-    })
-    
+    });
+
     return res.json(appointment);
   }
 }
