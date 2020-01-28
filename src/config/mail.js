ab59f1d3e68b92bbb645e3c0156fc84ce5b2a4c9
rlindoso <rafael.lindoso@gmail.com>
2020-01-28 13:50:59 -0300

    Variáveis ambiente

--- src/config/mail.js
+++ src/config/mail.js
@@ -1,10 +1,10 @@
 export default {
-  host: 'smtp.mailtrap.io',
-  port: '2525',
+  host: process.env.MAIL_HOST,
+  port: process.env.MAIL_PORT,
   secure: false,
   auth: {
-    user: '1858909312c43b',
-    pass: '7f000c80c9102b',
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS,
   },
   default: {
     from: 'Equipe GoBarber <noreply@gobarber.com>',
--- src/config/sentry.js
+++ src/config/sentry.js
@@ -1,3 +1,3 @@
 export default {
-  dsn: 'https://bc6db0d1944242399315b6d73ed7d364@sentry.io/2040565',
+  dsn: process.env.SENTRY_DNS,
 };
