67f90d34ef4063baf6d40754a9516a940da9ff82
Rafael Lindoso <rafael.lindoso@gmail.com>
2020-03-15 15:52:04 -0300

    Ajustes na API

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,6 +1,7 @@
 import * as Yup from 'yup';
 
 import User from '../models/User';
+import File from '../models/File';
 
 class UserController {
   async store(req, res) {
@@ -68,9 +69,19 @@ class UserController {
       return res.status(401).json({ error: 'Password does not match' });
     }
 
-    const { id, name, provider } = await user.update(req.body);
+    await user.update(req.body);
 
-    return res.json({ id, name, email, provider });
+    const { id, name, avatar } = await User.findByPk(req.userId, {
+      include: [
+        {
+          model: File,
+          as: 'avatar',
+          attributes: ['id', 'path', 'url'],
+        },
+      ],
+    });
+
+    return res.json({ id, name, email, avatar });
   }
 }
 
@@ -108,7 +119,7 @@ export default new UserController();
  *           email: rafael@email.com
  *           password: passwd
  *           provider: false
- * 
+ *
  *      UserUpdate:
  *        type: object
  *        properties:
@@ -138,7 +149,7 @@ export default new UserController();
  *           provider: false
  */
 
- /**
+/**
  * @swagger
  *  security:
  *    - bearerAuth: []         # use the same name as above
@@ -173,7 +184,7 @@ export default new UserController();
  *                $ref: '#/components/schema/User'
  */
 
- /**
+/**
  * @swagger
  * path:
  *  /users:
